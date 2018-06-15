32c703070428f01094ff2459457827d8f99a8331
Paul Mullins, Skrumble Ubuntu <paulmullins@protonmail.com>
2018-06-15 16:41:19 -0400

    Can now understand how to format json from 'ethers.Wallet.createRandom();' in a way that it works with 'ethers.Wallet.fromEncryptedWallet(json, password).then'

--- GenerateKeyPairs.js
+++ GenerateKeyPairs.js
@@ -6,17 +6,40 @@ var fs = require('fs');
 var argv = process.argv.slice(2);
 
 
-var KeyObject = {"mnemonic": ethers.Wallet.createRandom().mnemonic, "KeyPairs" : []}
+var KeyObject = {
+    "mnemonic": ethers.Wallet.createRandom().mnemonic,
+    "KeyPairs": []
+}
 
 
 console.log("Please wait while the key pairs are generated")
-for(var i = 0; i < argv[0]; i++){
-    var tmpWallet = ethers.Wallet.fromMnemonic(KeyObject.mnemonic, "m/00/"+i);
+for (var i = 0; i < argv[0]; i++) {
+    var tmpWallet = ethers.Wallet.fromMnemonic(KeyObject.mnemonic, "m/00/" + i);
     var tmpWalletObject = {};
     tmpWalletObject.privateKey = tmpWallet.privateKey;
     tmpWalletObject.address = tmpWallet.address;
     KeyObject.KeyPairs.push(tmpWalletObject);
-    if(i == argv[0] - 1){
+    var json = JSON.stringify(tmpWallet);
+    var password = "foo";
+    if (i < 10) {
+        ethers.Wallet.fromEncryptedWallet(json, password).then(function (wallet) {
+            console.log("Address: " + wallet.address);
+            ExportToJSON(wallet, "Wallet00" + i.toString() + ".json");
+        })
+    }
+    if (i < 100 && i > 9) {
+        ethers.Wallet.fromEncryptedWallet(json, password).then(function (wallet) {
+            console.log("Address: " + wallet.address);
+            ExportToJSON(wallet, "Wallet0" + i.toString() + ".json");
+        })
+    }
+    if (i < 1000 && i > 99) {
+        ethers.Wallet.fromEncryptedWallet(json, password).then(function (wallet) {
+            console.log("Address: " + wallet.address);
+            ExportToJSON(wallet, "Wallet" + i.toString() + ".json");
+        })
+    }
+    if (i == argv[0] - 1) {
         console.log("TEST")
         ExportToJSON(KeyObject, argv[1]);
     }
